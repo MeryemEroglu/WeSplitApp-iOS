2165ee7099546f106fdab8b54c45eb603d97f00d
MeryemEroglu <200101022@ogrenci.yalova.edu.tr>
2025-05-01 16:56:54 +0300

    Initial commit: Add WeSplit tip calculator functionality

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused : Bool
+    
+    let tipPercentages = [0, 10, 15, 20, 25]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipAmount = (checkAmount / 100) * tipSelection
+        let totalAmount = checkAmount + tipAmount
+        
+        let amountPerPerson = totalAmount / peopleCount
+        return amountPerPerson
+    }
+    
+    var totalAmount: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipAmount = (checkAmount / 100) * tipSelection
+        let totalAmount = checkAmount + tipAmount
+        
+        return totalAmount
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount,
+                              format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+
+                Picker("Number of people", selection: $numberOfPeople) {
+                    ForEach(2..<100, id: \.self) {
+                        Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section("How much do you want to tip?") {
+                    Picker("Tip Percentange", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Total amount") {
+                    Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .animation(.spring(), value: totalAmount)
+                }
+                
+                Section("Amount per person") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }.tint(.blue)
     }
 }
 
